ba2029460e78728d5963920796e807a24933aaf2
DOdrums <dirkornee@hotmail.com>
2022-08-15 11:17:44 +0000

    add base html and routes for extra html templates

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization (mobile menu) 
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
